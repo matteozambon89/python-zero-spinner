acbab50ad7f7d21854fbda2ba7b097ef2ca3fc04
Matteo Zambon <matteo@yeeti.co>
2025-12-07 12:57:29 +0000

    Fixed badges

--- src/yeeti/zero_spinner/__init__.py
+++ src/yeeti/zero_spinner/__init__.py
@@ -2,3 +2,13 @@
 from .main import Spinner, spinner
 
 __all__ = ['Spinner', 'spinner']
+
+__version__ = '0.0.0'  # Placeholder
+
+try:
+    from importlib.metadata import version
+
+    __version__ = version(__name__)
+except ImportError:
+    # Fallback for direct imports during dev
+    pass
